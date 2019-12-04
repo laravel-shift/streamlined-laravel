28808c549864da13d38af98cbbfb13e5426cc659
Jason McCreary <jason@pureconcepts.net>
2019-12-03 19:36:30 -0500

    Favor extended configuration

---
+++ bindings/config.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Configuration
+|--------------------------------------------------------------------------
+|
+| Here you may configure your application with custom options and values.
+| Your configuration will be merged with the Laravel defaults
+| so you may easily override core options as well.
+|
+*/
+
+return [
+
+
+
+];
