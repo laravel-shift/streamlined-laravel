35ce2212eb30858fc1ebfafa6ae3172250309593
Jason McCreary <jason@pureconcepts.net>
2019-12-03 19:59:14 -0500

    Favor middleware binding over kernel

---
+++ bindings/middleware.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| HTTP Middleware
+|--------------------------------------------------------------------------
+|
+| Here is where you can register middleware for your application. These
+| By default, Laravel registers core middleware and specifies the
+| order of this middleware. You may remove any core middleware
+| or add your own custom middleware.
+|
+*/
+
+return [
+
+    'global' => [
+        \App\Http\Middleware\TrustProxies::class,
+        \App\Http\Middleware\CheckForMaintenanceMode::class,
+        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+        \App\Http\Middleware\TrimStrings::class,
+        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+    ],
+
+
+    'web' => [
+        \App\Http\Middleware\EncryptCookies::class,
+        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        \Illuminate\Session\Middleware\StartSession::class,
+        // \Illuminate\Session\Middleware\AuthenticateSession::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \App\Http\Middleware\VerifyCsrfToken::class,
+        \Illuminate\Routing\Middleware\SubstituteBindings::class,
+    ],
+
+
+    'api' => [
+        'throttle:60,1',
+        'bindings',
+    ],
+
+
+    'application' => [
+
+        // ...
+
+    ],
+
+
+    'priority' => [
+        \Illuminate\Session\Middleware\StartSession::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \App\Http\Middleware\Authenticate::class,
+        \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        \Illuminate\Session\Middleware\AuthenticateSession::class,
+        \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        \Illuminate\Auth\Middleware\Authorize::class,
+    ],
+
+];
