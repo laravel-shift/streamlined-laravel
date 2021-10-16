3a96403280b452923fffc45a97e2316fa655d129
Jason McCreary <jason@pureconcepts.net>
2021-10-16 17:21:07 -0500

    Move Console/Kernel scheduling to existing config

--- routes/console.php
+++ routes/console.php
@@ -8,12 +8,24 @@ use Illuminate\Support\Facades\Artisan;
 | Console Routes
 |--------------------------------------------------------------------------
 |
-| This file is where you may define all of your Closure based console
-| commands. Each Closure is bound to a command instance allowing a
-| simple approach to interacting with each command's IO methods.
+| Here you may define all of your Closure based console commands.
+| Each Closure is bound to a command instance allowing a simple
+| approach to interacting with each command's IO methods.
 |
 */
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+/*
+|--------------------------------------------------------------------------
+| Console Schedule
+|--------------------------------------------------------------------------
+|
+| Here you may schedule any of your console commands to run.
+| You may pass the name of the command or a closure
+| to be run at a specific time or day.
+|
+*/
+Artisan::schedule('inspire')->hourly();
